fcad488db763d82147702d71843ce920994991dd
Abi Athmakuri <aathmakuri034@gmail.com>
2026-04-26 22:47:59 -0500

    Created createArrays.js

---
+++ createArrays.js
@@ -0,0 +1,7 @@
+let storeList = ["milk", "eggs", "chocolate", "hot sauce", "bread"];
+let pricesList = [4,5,6];
+
+console.log(storeList);
+console.log(pricesList);
+console.log(storeList.slice(0,2));
+console.log(pricesList.slice(0,1));
